f16b0f6b3f78ae83ea98ba4c87e6b620a82a0082
Travis Sein <travis@Traviss-Mac-Studio.local>
2024-07-30 14:58:13 -0700

    counts

--- functions/polls/[poll].ts
+++ functions/polls/[poll].ts
@@ -1,4 +1,4 @@
-import { Env, Poll, Option } from "../lib/types";
+import { Env, Poll } from "../lib/types";
 
 // Read a poll
 export const onRequestGet: PagesFunction<Env> = async (context) => {
@@ -6,15 +6,26 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
     const { poll: pollId } = context.params;
 
     const getPoll = `SELECT * FROM polls WHERE id = ?`;
-    const getOptions = `SELECT * FROM options WHERE poll_id = ?`;
+    const getOptionsWithResponses = `
+        SELECT options.id, options.text, options.image, COUNT(response_options.response_id) as response_count
+        FROM options
+        LEFT JOIN response_options ON options.id = response_options.option_id
+        WHERE options.poll_id = ?
+        GROUP BY options.id
+    `;
 
     try {
         const poll = await POLLS_DB.prepare(getPoll).bind(pollId).first();
         if (!poll) {
             return new Response('Poll not found', { status: 404 });
         }
-
-        const { results: options } = await POLLS_DB.prepare(getOptions).bind(pollId).all();
+        const optionsResult = await POLLS_DB.prepare(getOptionsWithResponses).bind(pollId).all();
+        const options = optionsResult.results.map((option: any) => ({
+            id: option.id,
+            text: option.text,
+            image: option.image,
+            response_count: option.response_count
+        }));
         return new Response(JSON.stringify({ poll, options }), { status: 200 });
     } catch (error) {
         return new Response(`Error reading poll: ${error.message}`, { status: 500 });
