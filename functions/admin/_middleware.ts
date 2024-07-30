f7711df79d505388d0bc1478636e0fbd7b0da91e
Travis Sein <travis@Traviss-Mac-Studio.local>
2024-07-30 10:22:36 -0700

    basic

--- functions/admin/_middleware.ts
+++ functions/admin/_middleware.ts
@@ -3,7 +3,15 @@ export async function onRequest(context) {
     const { request, env } = context;
     const authHeader = request.headers.get("Authorization");
 
-    if (!authHeader || authHeader !== `Bearer ${env.API_KEY}`) {
+    if (!authHeader || !authHeader.startsWith("Basic ")) {
+        return new Response('Unauthorized', { status: 401 });
+    }
+
+    const base64Credentials = authHeader.split(" ")[1];
+    const credentials = atob(base64Credentials).split(":");
+    const apiKey = credentials[1];
+
+    if (apiKey !== env.API_KEY) {
         return new Response('Unauthorized', { status: 401 });
     }
 
