1945e0afa126916727e5659251cbe7c8e1f24da3
Travis Sein <travis134@gmail.com>
2024-08-01 07:43:19 -0700

    dam

--- functions/api/admin/polls.ts
+++ functions/api/admin/polls.ts
@@ -10,12 +10,11 @@ export const onRequestPost: PagesFunction<Env> = async (context) => {
         throw new BadRequest();
     }
 
-    const { name, description, selections } = poll;
+    const { id, name, description, selections } = poll;
     const createPoll = `INSERT INTO polls (id, name, description, selections) VALUES (?, ?, ?, ?)`;
     const result = await POLLS_DB.prepare(createPoll)
-        .bind(name, description, selections)
+        .bind(id, name, description, selections)
         .run();
-    const id = result.meta.last_row_id;
 
     return Response.json({ id });
 };
