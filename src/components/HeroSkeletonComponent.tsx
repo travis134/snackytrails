b5f98130ce813ba1b100eba2ba40ed3e58f71d8c
Travis Sein <travis134@gmail.com>
2024-08-20 17:21:08 -0700

    Update HeroSkeletonComponent.tsx

--- src/components/HeroSkeletonComponent.tsx
+++ src/components/HeroSkeletonComponent.tsx
@@ -4,10 +4,10 @@ const HeroSkeletonComponent: React.FC = () => {
     return (
         <section className="hero">
             <div className="hero-body">
-                <p className="title skeleton-lines">
+                <p className="title skeleton-lines mb-2">
                     <div />
                 </p>
-                <p className="subtitle skeleton-lines">
+                <p className="subtitle skeleton-lines mb-2">
                     <div />
                 </p>
             </div>
